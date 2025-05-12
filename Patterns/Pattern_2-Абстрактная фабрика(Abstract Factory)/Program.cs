5803b59f38d8a3bd5fbc727d300c22a4f2bcddba
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-05-12 03:44:31 +0300

    1.0.2

--- Patterns/Pattern_2-Абстрактная фабрика(Abstract Factory)/Program.cs
+++ Patterns/Pattern_2-Абстрактная фабрика(Abstract Factory)/Program.cs
@@ -1,4 +1,12 @@
 ﻿/*
+ * Author: Nikolay Dvurechensky
+ * Site: https://www.dvurechensky.pro/
+ * Gmail: dvurechenskysoft@gmail.com
+ * Last Updated: 12 мая 2025 03:39:52
+ * Version: 1.0.4
+ */
+
+/*
  * ПОРОЖДАЮЩИЕ ПАТТЕРНЫ
  * 
  * Глава_9: Абстрактная фабрика (Abstract Factory)
--- Patterns/Pattern_9-Цепочка обязанностей/Program.cs
+++ Patterns/Pattern_9-Цепочка обязанностей/Program.cs
@@ -1,4 +1,12 @@
 ﻿/*
+ * Author: Nikolay Dvurechensky
+ * Site: https://www.dvurechensky.pro/
+ * Gmail: dvurechenskysoft@gmail.com
+ * Last Updated: 12 мая 2025 03:39:52
+ * Version: 1.0.4
+ */
+
+/*
  * ПАТТЕРНЫ ПОВЕДЕНИЯ
  * 
  * Глава_7_3: Цепочка обязанностей
