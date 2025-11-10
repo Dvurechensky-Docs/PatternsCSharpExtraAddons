d96bd1b74a87728e0b61f8b38f35dd207c6fe219
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-10 06:52:13 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-10

--- Patterns/Pattern_1-Стратегия/ILogReader.cs
+++ Patterns/Pattern_1-Стратегия/ILogReader.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 ноября 2025 06:51:41
- * Version: 1.0.82
+ * Last Updated: 10 ноября 2025 06:52:09
+ * Version: 1.0.83
  */
 
 
