26a4f11fb989999ab84ab0adcdc2f9ea4f6b94d5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-11 17:09:14 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-11

--- Patterns/Pattern_5-Наблюдатель(Observer)/SimpleObserver.cs
+++ Patterns/Pattern_5-Наблюдатель(Observer)/SimpleObserver.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://www.dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 января 2026 06:51:46
- * Version: 1.0.144
+ * Last Updated: 11 января 2026 17:09:11
+ * Version: 1.0.145
  */
 
 using System;
