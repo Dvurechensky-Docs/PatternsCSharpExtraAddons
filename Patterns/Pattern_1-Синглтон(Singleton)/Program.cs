fc8fb6be1b2b3c6d0bee8c17c606918c871926bf
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-11 14:00:13 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-11

--- Patterns/Pattern_1-Синглтон(Singleton)/Program.cs
+++ Patterns/Pattern_1-Синглтон(Singleton)/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 июля 2026 11:53:52
- * Version: 1.0.328
+ * Last Updated: 11 июля 2026 14:00:08
+ * Version: 1.0.329
  */
 
 /*
