6d129bc8cbb0ccb182b93fb3823c4396d4bba414
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-31 16:26:09 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-31

--- Patterns/Pattern_2-Шаблонный метод/ExceptionLogEntry.cs
+++ Patterns/Pattern_2-Шаблонный метод/ExceptionLogEntry.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 июля 2026 06:51:33
- * Version: 1.0.348
+ * Last Updated: 31 июля 2026 16:26:04
+ * Version: 1.0.349
  */
 
 namespace Creational;
